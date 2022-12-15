c29027b9040105757ff09378a91bd554a083efcf
SUMMAIYA <103142498+Summaiya-Begum@users.noreply.github.com>
2022-12-16 02:11:01 +0530

    api

--- routes/products.js
+++ routes/products.js
@@ -4,22 +4,20 @@ const ProductModel = require("../models/productSchema");
 const productRouter = Router();
 
 productRouter.get("/", async (req, res) => {
-  const query = req.query
-  console.log(query.q)
-  if(query){
-    if(query.q=='asc'){
-      const result = await ProductModel.find().sort({createdAt: 1})
-      return res.send(result)
-    }else{
-      const result = await ProductModel.find().sort({createdAt: -1})
-      return res.send(result)
+  const query = req.query;
+  console.log(query.q);
+  if (query) {
+    if (query.q == "asc") {
+      const result = await ProductModel.find().sort({ createdAt: 1 });
+      return res.send({ msg: "Welcome Product Home Page", product: result });
+    } else {
+      const result = await ProductModel.find().sort({ createdAt: -1 });
+      return res.send({ msg: "Welcome Product Home Page", product: result });
     }
   }
   const product = await ProductModel.find();
   res.send({ msg: "Welcome Product Home Page", product });
-
 });
-
 
 productRouter.post("/create", async (req, res) => {
   const { title, quantity, priority, description } = req.body;
@@ -39,14 +37,15 @@ productRouter.post("/create", async (req, res) => {
 });
 
 // Product EDIT CRUD
-productRouter.patch("/edit/:id", async(req, res) => {
-    const id = req.params.id
-    // console.log(id)
-    const payload = req.body
-    const updateProduct =  await ProductModel.findByIdAndUpdate(id,payload,{ new: true })
-    await updateProduct.save()
-    res.send({msg:"Data Update Successfully", updateProduct})
-
+productRouter.patch("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  // console.log(id)
+  const payload = req.body;
+  const updateProduct = await ProductModel.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  await updateProduct.save();
+  res.send({ msg: "Data Update Successfully", updateProduct });
 });
 
 // Product DELETE CRUD
@@ -55,6 +54,5 @@ productRouter.delete("/delete/:id", async (req, res) => {
   await ProductModel.findByIdAndDelete({ _id: id });
   res.send({ msg: "Product Deleted Successfully" });
 });
-
 
 module.exports = productRouter;
